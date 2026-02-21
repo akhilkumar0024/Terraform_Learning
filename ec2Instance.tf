b25574dca933749d5c56a3683fc59f32f644263c
akhilkumar <akhilkumar0024.devops@gmail.com>
2026-02-21 05:38:56 +0530

    updated the code to have ec2 instances for prometheus and graffana

--- ec2Instance.tf
+++ ec2Instance.tf
@@ -14,20 +14,79 @@ data "aws_ami" "amazon_linux_image" {
   owners = ["137112412989"] # Amazon
 }
 
-resource "aws_instance" "amazon_linux_instance" {
+# Docker Prometheus Server
+resource "aws_instance" "Docker_Prometheus_Server" {
   ami           = data.aws_ami.amazon_linux_image.id
-  instance_type = "t3.micro"
-
+  instance_type = "t3.small"
+  key_name = "dockerKeyPair"
+  vpc_security_group_ids = [aws_security_group.docker_prometheus_server_sg.id]
   tags = {
-    Name = "amazon_linux_instance_terraform_demo"
+    Name = "Docker_Prometheus_Server"
   }
 }
 
-resource "aws_s3_bucket" "s3_demo_bucket" {
-  bucket = "my-tf-test-bucket-akhilkumar0024"
-
+# Graffana Monitoring Server
+resource "aws_instance" "Graffana_Monitoring_Server" {
+  ami           = data.aws_ami.amazon_linux_image.id
+  instance_type = "t3.small"
+  key_name = "dockerKeyPair"
+  vpc_security_group_ids = [aws_security_group.graffana_server_sg.id]
   tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
+    Name = "Graffana_Monitoring_Server"
+  }
+}
+
+
+# Security Group for Graffana Server
+resource "aws_security_group" "graffana_server_sg" {
+  name        = "graffana-server-sg"
+  description = "Security group for Graffana server"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Security Group for Docker Server
+resource "aws_security_group" "docker_prometheus_server_sg" {
+  name        = "docker-prometheus-server-sg"
+  description = "Security group for Docker and Prometheus server"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
